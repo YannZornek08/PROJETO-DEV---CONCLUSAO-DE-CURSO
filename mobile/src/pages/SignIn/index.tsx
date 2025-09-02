33e0e406204399a7b4a43bdffc0dbfb5374189cd
Teste <guilherme.f.rodrigues7@aluno.senai.br>
2025-09-02 16:54:00 -0300

    Inicio Tela de Login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,105 +1,58 @@
-import React, { useState, useContext } from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    TextInput,
-    TouchableOpacity,
-    ActivityIndicator
-} from "react-native";
-
-import { AuthContext } from "../../contexts/AuthContext";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function SignIn() {
-    const { signIn, loadingAuth } = useContext(AuthContext);
+  return (
+    <View>
+    <Text style={styles.container}>Login</Text>
+    <Div />
+    </View>
+  );
+}
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    async function handleLogin() {
-
-        if (email === '' || password === '') {
-            return;
-        }
-        await signIn({email, password});
-    }
-
-    return (
-        <View style={styles.container}>
-            <Image
-                style={styles.logo}
-                source={require('../../assets/logo.png')}
-            />
-
-
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Digite o seu e-mail"
-                    style={styles.input}
-                    placeholderTextColor="#f0f0f0"
-                    value={email}
-                    onChangeText={setEmail}
-
-                />
-                <TextInput
-                    placeholder="Sua senha"
-                    style={styles.input}
-                    placeholderTextColor="#f0f0f0"
-                    secureTextEntry={true}
-                    value={password}
-                    onChangeText={setPassword}
-                />
-                <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                    { loadingAuth ? (
-                        <ActivityIndicator size={25} color="#101026" />
-                    ) : (
-                        <Text style={styles.buttonText}>Acessar</Text>
-                    )}
-                </TouchableOpacity>
-            </View>
-
-        </View>
-    )
+function Div() {
+  return (
+    <View>
+      <View style={styles.div}>
+    <Text style={styles.text_title}>Email</Text>
+    <TextInput placeholder="Exemplo: exemplo@gmail.com" style={styles.text_placeholder}/>
+    <Text style={styles.text_title}>Senha:</Text>
+    <TextInput placeholder="Senha@Secreta2" style={styles.text_placeholder} secureTextEntry={true}/>
+      </View>
+    <Text style={styles.space_google}/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#1D1D2E'
+        fontSize: 32,
+        marginTop: 75,
+        marginBottom: 25,
+        textAlign: 'center',
     },
-    logo: {
-        marginBottom: 18
+    div: {
+      backgroundColor: '#F1E0CA',
+      borderRadius: 10,
+      margin: 20,
+      padding: 20,
+      borderColor: '#D6C4AA',
+      borderWidth: 2,
     },
-    inputContainer: {
-        width: '95%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 32,
-        paddingHorizontal: 14
+    text_title: {
+      fontSize: 22,
+      marginBottom: 10,
     },
-    input: {
-        width: '95%',
-        height: 40,
-        backgroundColor: '#101026',
-        marginBottom: 12,
-        borderRadius: 4,
-        paddingHorizontal: 8,
-        color: '#FFF'
+    text_placeholder: {
+      fontSize: 14,
+      backgroundColor: '#FFFFFF',
+      marginBottom: 15,
+      borderBottomColor: '#423828',
+      borderBottomWidth: 1,
     },
-    button: {
-        width: '95%',
-        height: 40,
-        backgroundColor: '#3FFFA3',
-        borderRadius: 4,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#101026'
+    space_google: {
+      backgroundColor: '#FFFFFF',
+      
     }
-})
+});
