177d3de65d68a8807d19d0e85701d968b9d41509
Leornardo Moraes <leonardo.l.moraes@aluno.senai.br>
2025-09-16 13:35:36 -0300

    Subindo alterações para dar Push

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: "http://localhost:3333"
-    baseURL: 'http://10.149.175.167:3333'
+    baseURL: 'http://10.109.221.141:3333'
 })
 
 export { api }
