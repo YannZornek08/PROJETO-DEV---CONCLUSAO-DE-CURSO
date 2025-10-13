0aefa725576e69652981328c1ed6d3990c732e67
Vinicius Domingos <vinicius.d.jesus@aluno.senai.br>
2025-10-13 17:25:09 -0300

    :construction: Concluindo QRCODE

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: "http://localhost:3333"
-    baseURL: 'http://10.106.131.39:3333'
+    baseURL: 'http://192.168.88.8:3333'
 })
 
 export { api }
